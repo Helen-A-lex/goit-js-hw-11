b3769e682cab4301d769ed4ad85bf1b904cccf26
Helen-A-lex <elenahomutovskaa652@gmail.com>
2023-05-05 20:15:47 +0200

    commit try

--- src/js/onLoad.js
+++ src/js/onLoad.js
@@ -1,38 +1,42 @@
-import { renderPictureCard } from './renderPictureCard';
-import { getPictures } from './getPictures';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-import refs from './refs';
-let searchQuery = '';
-let page = 1;
-const perPage = 40;
+// import { renderPictureCard } from './renderPictureCard';
+// import { getPictures } from './getPictures';
+// import { onFormSearch } from './onFormSearch';
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
 
-// Нескінченний скрол
-let options = {
-  root: null,
-  rootMargin: '300px',
-  threshold: 1.0,
-};
-let observer = new IntersectionObserver(onLoad, options);
+// import refs from './refs';
+// let searchQuery = '';
+// let page = 1;
+// let perPage = 40;
 
-let allPicturesLoaded = false;
-let totalHits = 0;
+// // Нескінченний скрол
+// let options = {
+//   root: null,
+//   rootMargin: '300px',
+//   threshold: 1.0,
+// };
 
-export function onLoad(entries) {
-  entries.forEach(entry => {
-    if (entry.isIntersecting && perPage < totalHits) {
-      page += 1;
-      getPictures(searchQuery, page, perPage)
-        .then(data => {
-          renderPictureCard(data.hits);
-          SimpleLightbox.refresh();
-          perPage += data.hits.length;
-          if (perPage >= totalHits) {
-            allPicturesLoaded = true;
-            refs.btnLoadMore.hidden = true;
-          }
-        })
-        .catch(error => console.log(error));
-    }
-  });
-}
+// let observer = new IntersectionObserver(onLoad, options);
+
+// let allPicturesLoaded = false;
+// let totalHits = 0;
+
+// export function onLoad(entries) {
+//   entries.forEach(entry => {
+//     if (entry.isIntersecting && perPage < totalHits) {
+//       page += 1;
+//       getPictures(searchQuery, page, perPage)
+//         .then(data => {
+//           renderPictureCard(data.hits);
+//           SimpleLightbox.refresh();
+//           perPage += data.hits.length;
+
+//           if (perPage >= totalHits) {
+//             allPicturesLoaded = true;
+//             refs.btnLoadMore.hidden = true;
+//           }
+//         })
+//         .catch(error => console.log(error));
+//     }
+//   });
+// }
--- src/js/renderPictureCard.js
+++ src/js/renderPictureCard.js
@@ -1,42 +1,42 @@
-import refs from './refs';
-import { getPictures } from './getPictures';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+// import refs from './refs';
+// import { getPictures } from './getPictures';
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
 
-export function renderPictureCard(pictures) {
-  const markup = pictures
-    .map(
-      ({
-        id,
-        largeImageURL,
-        webformatURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => `<a class="gallery__link" href="${largeImageURL}"data-lb-group="search-results">
-      <div class="gallery-wrapper" id="${id}">
-           <img class="gallery__img" src="${webformatURL}" alt="${tags}" loading="lazy"  />
-            <div class="info">
-              <p class="info-item"><b>Likes</b> ${likes}</p>
-              <p class="info-item"><b>Views</b> ${views}</p>
-              <p class="info-item"><b>Comments</b> ${comments}</p>
-              <p class="info-item"><b>Downloads</b> ${downloads}</p>
-            </div>
-          </div>
-          </a>`
-    )
-    .join('');
-  refs.pictureContainer.insertAdjacentHTML('beforeend', markup);
-  const simpleLightBox = new SimpleLightbox('.gallery__link');
+// export function renderPictureCard(pictures) {
+//   const markup = pictures
+//     .map(
+//       ({
+//         id,
+//         largeImageURL,
+//         webformatURL,
+//         tags,
+//         likes,
+//         views,
+//         comments,
+//         downloads,
+//       }) => `<a class="gallery__link" href="${largeImageURL}"data-lb-group="search-results">
+//       <div class="gallery-wrapper" id="${id}">
+//            <img class="gallery__img" src="${webformatURL}" alt="${tags}" loading="lazy"  />
+//             <div class="info">
+//               <p class="info-item"><b>Likes</b> ${likes}</p>
+//               <p class="info-item"><b>Views</b> ${views}</p>
+//               <p class="info-item"><b>Comments</b> ${comments}</p>
+//               <p class="info-item"><b>Downloads</b> ${downloads}</p>
+//             </div>
+//           </div>
+//           </a>`
+//     )
+//     .join('');
+//   refs.pictureContainer.insertAdjacentHTML('beforeend', markup);
+//   const simpleLightBox = new SimpleLightbox('.gallery__link');
 
-  const { height: cardHeight } = document
-    .querySelector('.gallery')
-    .firstElementChild.getBoundingClientRect();
+//   const { height: cardHeight } = document
+//     .querySelector('.gallery')
+//     .firstElementChild.getBoundingClientRect();
 
-  window.scrollBy({
-    top: cardHeight * 2,
-    behavior: 'smooth',
-  });
-}
+//   window.scrollBy({
+//     top: cardHeight * 2,
+//     behavior: 'smooth',
+//   });
+// }
